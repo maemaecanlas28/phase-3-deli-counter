a4f3ae54070fecee340b5302589391c9fa9cae8f
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-09 20:26:45 -0700

    commit tree d334d5b300c81c007ea6e20f0f988544e8220dc2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+katz_deli = []
+
+def line (list) 
+    if list.size == 0 
+        return puts "The line is currently empty."
+    end
+    sentence = "The line is currently:"
+    list.map.with_index(1) do |name, position|
+        sentence += " #{position}. #{name}"
+    end
+    return puts sentence
+end
+
+def take_a_number (list, name)
+    list.push(name)
+    puts "Welcome, #{name}. You are number #{list.size} in line."
+end
+
+def now_serving (list)
+    if list.size == 0
+        return puts "There is nobody waiting to be served!"
+    end
+    puts "Currently serving #{list[0]}."
+    list.shift()
+end
